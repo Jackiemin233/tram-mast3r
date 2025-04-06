7500b495a84e80af942e8fa4077c80ff15ac2893
Jackiemin233 <328234069@qq.com>
2025-04-06 13:13:17 +0000

    commit

---
+++ thirdparty/MASt3R-SLAM/run_masked_slam.py
@@ -0,0 +1,27 @@
+import argparse
+import datetime
+import pathlib
+import sys
+import time
+import cv2
+import lietorch
+import torch
+import tqdm
+import yaml
+from mast3r_slam.global_opt import FactorGraph
+
+from mast3r_slam.config import load_config, config, set_global_config
+from mast3r_slam.dataloader import Intrinsics, load_dataset
+import mast3r_slam.evaluate as eval
+from mast3r_slam.frame import Mode, SharedKeyframes, SharedStates, create_frame
+from mast3r_slam.mast3r_utils import (
+    load_mast3r,
+    load_retriever,
+    mast3r_inference_mono,
+)
+from mast3r_slam.multiprocess_utils import new_queue, try_get_msg
+from mast3r_slam.tracker import FrameTracker
+from mast3r_slam.visualization import WindowMsg, run_visualization
+import torch.multiprocessing as mp
+
+
